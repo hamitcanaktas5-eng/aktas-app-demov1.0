f86aca06b3443588ab6f843f7fef44684725d440
hamitcanaktas5-eng <hamitcanaktas5@gmail.com>
2025-12-14 13:34:55 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,26 +1,9 @@
-const menuBtn = document.getElementById("menuBtn");
-const sideMenu = document.getElementById("sideMenu");
-const channelList = document.getElementById("channelList");
-const mainPanel = document.getElementById("mainPanel");
+window.onload = () => {
+  const loggedIn = localStorage.getItem("loggedIn");
 
-menuBtn.onclick = () => {
-  sideMenu.classList.toggle("open");
+  if (loggedIn === "true") {
+    showMain();
+  } else {
+    showLogin();
+  }
 };
-
-document.getElementById("addChannel").onclick = () => {
-  const name = prompt("Kanal adı:");
-  if (!name) return;
-
-  const div = document.createElement("div");
-  div.textContent = "#" + name;
-  div.onclick = () => openChannel(name);
-  channelList.appendChild(div);
-};
-
-function openChannel(name) {
-  sideMenu.classList.remove("open");
-  mainPanel.innerHTML = `
-    <h2>#${name}</h2>
-    <input placeholder="Mesaj yaz..." />
-  `;
-}
